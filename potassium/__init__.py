551a7aaee09d4cf5abc513cbbb99c693d55d4273
David Leon Gil <coruus@gmail.com>
2014-08-18 18:50:39 -0400

    Rearranged directory structure.

---
+++ potassium/__init__.py
@@ -0,0 +1,1 @@
+from potassium.curve25519_aes256_hmacsha512 import write_keypair, get_pubkey, decrypt_withkeyfile, encrypt_topubkey, curve25519_aes_ctr_hmac_sha512_encrypt, curve25519_aes_ctr_hmac_sha512_decrypt
